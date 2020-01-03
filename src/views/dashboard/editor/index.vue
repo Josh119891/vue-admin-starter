c6ad84485ed5e616528e738fc9b7d98491c7de6c
josh119891 <josh119891@hotmail.com>
2020-01-03 14:05:23 +0800

    starter

--- src/views/dashboard/editor/index.vue
+++ src/views/dashboard/editor/index.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="dashboard-editor-container">
     <div class=" clearfix">
-      <pan-thumb :image="avatar" style="float: left">
-        Your roles:
-        <span v-for="item in roles" :key="item" class="pan-info-roles">{{ item }}</span>
-      </pan-thumb>
       <div class="info-container">
-        <span class="display_name">{{ name }}</span>
-        <span style="font-size:20px;padding-top:20px;display:inline-block;">Editor's Dashboard</span>
+        <span style="font-size:60px;display:inline-block;">Editor's Dashboard</span>
       </div>
     </div>
-    <div>
-      <img :src="emptyGif" class="emptyGif">
-    </div>
+
   </div>
 </template>
 
@@ -23,7 +16,6 @@ export default {
   name: 'DashboardEditor',
   data() {
     return {
-      emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
     }
   },
   computed: {
@@ -37,16 +29,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .emptyGif {
-    display: block;
-    width: 45%;
-    margin: 0 auto;
-  }
-
   .dashboard-editor-container {
     background-color: #e3e3e3;
     min-height: 100vh;
-    padding: 50px 60px 0px;
+    padding: 0px 40px 0px;
     .pan-info-roles {
       font-size: 12px;
       font-weight: 700;
@@ -55,16 +41,8 @@ export default {
     }
     .info-container {
       position: relative;
-      margin-left: 190px;
       height: 150px;
-      line-height: 200px;
-      .display_name {
-        font-size: 48px;
-        line-height: 48px;
-        color: #212121;
-        position: absolute;
-        top: 25px;
-      }
+      line-height: 150px;
     }
   }
 </style>
